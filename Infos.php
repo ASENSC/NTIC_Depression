17f30935f07c884685ee0df062d1ecba18476eb8
Alexandre Senaux <asenaux@ensc.fr>
2017-11-07 15:09:02 +0100

    Architecture

---
+++ Infos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div name="SantePublique">
+            <h2>La depression - Problème de santé publique</h2>
+            <p>A remplir</p>
+        </div>
+        
+        <div name="ComprendreDepression">
+            <h2>Comprendre la dépression</h2>
+            <p>A remplir</p>
+        </div>
+        
+        <div name="PriseEnCharge">
+            <h2>Prise en charge de la dépression</h2>
+            <p>A remplir</p>
+        </div>
+        
+        <div name="Prevention">
+            <h2>Depression et prévention</h2>
+            <p>A remplir</p>
+        </div>
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>NTIC_Depression</title>
+    </head>
+    <body>
+        <a type=button href="./Infos.php">
+            <button name="btnInfo">Informations</button>
+        </a>
+        
+        <a type=button href="./Diagnostique.php">
+            <button name="btnDiag">Diagnostique</button>
+        </a>
+        
+        <a type=button href="./Contact.php">
+            <button name="btnContact">Contact</button>
+        </a>
+    </body>
+</html>
